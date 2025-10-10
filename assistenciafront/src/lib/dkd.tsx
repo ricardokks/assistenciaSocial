5351328f55746d4509f677c0478dc9f7ba0cfb48
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-10-10 15:53:04 -0300

    feat:tailwind instalation

--- assistenciafront/src/lib/dkd.tsx
+++
@@ -1,9 +0,0 @@
-import { obj } from "../constants/oi";
-
-export default function Name() {
-    return(
-        <div className="">
-            <h1>{obj.nome}</h1>
-        </div>
-    )
-}
--- assistenciafront/src/routes/main.tsx
+++ assistenciafront/src/routes/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import "../lib/global.css"
+
 import { routes } from './routes';
 import { RouterProvider } from 'react-router-dom';
 
