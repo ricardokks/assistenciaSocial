59f153d1bafd8a208cee6c2821ac61f48547eec2
Felipe Lima <flimalimafelipek@gmail.com>
2025-10-14 13:30:39 -0300

    feat: .

--- assistenciafront/src/pages/home-page/index.tsx
+++ assistenciafront/src/pages/home-page/index.tsx
@@ -2,7 +2,7 @@ import { SectionHero } from './sections/hero'
 
 export function HomePage() {
   return (
-    <main className="background-gradient overflow-y-hidden flex h-screen w-full items-center justify-center">
+    <main className="background-gradient flex h-auto w-full items-center justify-center">
       <section className="m-0 min-h-[100vh] h-[100%] w-[100%]  max-w-[1280px] bg-transparent p-4 px-5">
         <SectionHero />
       </section>
