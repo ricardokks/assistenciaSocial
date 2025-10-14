e1a51231e856bdfd86131d424ab126ffdb5be90e
artizin9 <reis08897@gmail.com>
2025-10-14 16:02:43 -0300

    feat: update login-page

---
+++ src/assets/Icons/iconeSenha.tsx
@@ -0,0 +1,18 @@
+import type { TypeClassIcon } from "../../types/type-class-icon";
+
+export function IconeSenha({ className }: TypeClassIcon){
+    return (
+           <svg className={className} width="22" height="22" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_25_438)">
+                    <path d="M21.25 14.25H8.75C7.36929 14.25 6.25 15.3693 6.25 16.75V24.25C6.25 25.6307 7.36929 26.75 8.75 26.75H21.25C22.6307 26.75 23.75 25.6307 23.75 24.25V16.75C23.75 15.3693 22.6307 14.25 21.25 14.25Z" stroke="#194A99" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M15 21.75C15.6904 21.75 16.25 21.1904 16.25 20.5C16.25 19.8096 15.6904 19.25 15 19.25C14.3096 19.25 13.75 19.8096 13.75 20.5C13.75 21.1904 14.3096 21.75 15 21.75Z" stroke="#194A99" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M10 14.25V9.25C10 7.92392 10.5268 6.65215 11.4645 5.71447C12.4021 4.77678 13.6739 4.25 15 4.25C16.3261 4.25 17.5979 4.77678 18.5355 5.71447C19.4732 6.65215 20 7.92392 20 9.25V14.25" stroke="#194A99" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_25_438">
+                      <rect width="30" height="30" fill="white" transform="translate(0 0.5)" />
+                    </clipPath>
+                  </defs>
+                </svg>
+    )
+}
