eb0840a7eda4abfa19c00324e52d4b7681d27b61
Felipe Lima <flimalimafelipek@gmail.com>
2025-10-29 13:13:17 -0300

    feat: update in section funcionario

--- src/components/Inicio/InicioRoot.tsx
+++ src/components/Inicio/InicioRoot.tsx
@@ -2,7 +2,7 @@ import type { InterfaceChildren } from "../../types/interface-children";
 
 export function InicioRoot({ children }: InterfaceChildren){
     return (
-        <div className="mt-2 h-full w-[95%] px-4 py-0 max-md:w-full">
+        <div className="mt-2 h-full  w-[95%] px-4 py-0 max-md:w-full max-md:pb-32">
             {children}
         </div>
     )
