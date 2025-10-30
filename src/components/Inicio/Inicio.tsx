6d4025c1e42d668a0bb6a728b46324c8e068ff67
Felipe Lima <flimalimafelipek@gmail.com>
2025-10-30 13:41:10 -0300

    feat: update in section

--- src/components/Inicio/Inicio.tsx
+++ src/components/Inicio/Inicio.tsx
@@ -7,7 +7,7 @@ import { HeaderDashboards } from '../Header'
 
 export function Inicio({ user }: IHomeProps) {
   return (
-    <main className="flex h-full w-[calc(100%-20%)] pr-4 flex-col items-start space-y-6 max-md:px-4 max-md:w-full">
+    <main className="main">
       {/* Header da aplicação  */}
       <HeaderDashboards.root>
         <HeaderDashboards.perfil user={user} />
