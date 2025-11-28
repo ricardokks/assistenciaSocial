049c0eb27037c41536291db122c9b70e06a0f671
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2025-11-28 13:03:45 -0300

    .

--- src/utils/verify-role.ts
+++ src/utils/verify-role.ts
@@ -1,8 +1,8 @@
 import type { TypeUsario } from '../types/type-usuarios'
 import type { NavigateFunction } from 'react-router-dom'
 
-export function verifyRole(papel: TypeUsario, navigate: NavigateFunction, id: string ) {
-  if (papel === 'ADMINISTRADOR') navigate(`/dashboard/administrador/${id}`)
+export function verifyRole(papel: TypeUsario, navigate: NavigateFunction, id?: string ) {
+  if (papel === 'ADMINISTRADOR') navigate(`/dashboard/administrador/`)
   if (papel === 'CIDADAO') navigate(`/dashboard/cidadao/${id}`)
   if (papel === 'GESTOR') navigate(`/dashboard/gestor/${id}`)
   if (papel === 'FUNCIONARIO') navigate(`/dashboard/funcionario/`)
