7a189a18c032b6e295f0a1cb8368f3ad1eea4493
Paulo Ricardo <ricardoduartedonascimento@gmail.com>
2026-01-08 15:25:33 -0300

    fix: home-instituicao

--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://api.assistenciasocialmassape.com.br',
   withCredentials: true,
 })
 
