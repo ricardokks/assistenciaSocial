d9cf73c581c1db269af56899963afe728dd49917
artizin9 <reis08897@gmail.com>
2026-01-12 23:23:02 -0300

    não aguento mais

--- src/pages/home-cidadao/section/agendamento.tsx
+++ src/pages/home-cidadao/section/agendamento.tsx
@@ -74,10 +74,7 @@ export function Agendamento(user: {
       <div
         className={`text-primary-800 font-outfit flex w-full max-md:flex-col max-md:space-y-1 md:items-center md:justify-between ${isAnimateSearch ? 'max-md:hidden' : ''}`}
       >
-        <h1 className="text-2xl font-medium max-md:text-xl">
-          Meus Agendamentos
-        </h1>
-
+        <h1 className="text-2xl font-medium max-md:text-xl">Meus Agendamentos</h1>
         <button
           className="bg-primary-800 hover:bg-primary-800/90 flex cursor-pointer items-center rounded-lg px-4 py-2 text-white shadow-md duration-500 hover:shadow-lg max-md:w-1/2 max-md:max-w-[170px] max-md:px-2 max-md:py-1 max-md:text-sm"
           onClick={() => setVisibilidadeModalCriarAgendamento(true)}
@@ -87,41 +84,43 @@ export function Agendamento(user: {
         </button>
       </div>
 
-      {/* SEARCH */}
+      {/* Search */}
       <div
-        className={`relative flex w-[80%] items-center text-center max-xl:w-4/5 max-lg:w-full max-md:w-full ${isAnimateSearch ? 'max-md:hidden' : ''
-          }`}
+        className={`relative flex w-[80%] items-center text-center max-2xl:-translate-y-3 max-xl:w-4/5 max-xl:-translate-y-0 max-lg:w-full max-md:w-full ${isAnimateSearch ? 'max-md:hidden' : ''}`}
       >
-        <IconeSearch className="absolute left-3 top-[1rem]" />
-
+        {/* Icone search */}
+        <IconeSearch className="absolute left-3 top-[1.25rem]" />
         <input
-          className="text-primary-800 border-primary-800 font-outfit ml-1 size-full appearance-none rounded-2xl border-2 bg-transparent pl-5 text-[16px] shadow-md outline-none"
+          className="font-satoshi border-primary-800 text-primary-800 placeholder:text-primary-800/65 placeholder:font-satoshi mt-3  size-full rounded-2xl border-2 px-2 py-1 pl-10 shadow shadow-black/10 outline-none outline-0 max-xl:pl-10 max-md:w-3/5"
           placeholder="Procure pelo nome..."
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-
-        <div className="relative ml-3 mt-2 h-full w-1/4 max-md:w-3/5">
+        <div className="relative ml-3 mt-3 h-full w-1/4 max-md:w-2/5">
           <select
-            className="border-primary-800 text-primary-800 size-full appearance-none rounded-2xl border-2 bg-transparent pl-3 shadow outline-none"
+            className="text-primary-800 border-primary-800 font-outfit ml-1 size-full appearance-none rounded-2xl border-2  bg-transparent pl-3 text-[16px] shadow-md outline-none"
             value={selectedStatus}
             onChange={(e) => setSelectedStatus(e.target.value)}
-            onMouseDown={() => setIsAnimate(true)}
+            onClick={() => setIsAnimate((prev) => !prev)}
+            onMouseDown={() => setIsAnimate((prev) => !prev)}
+            onMouseLeave={() => setIsAnimate(false)}
             onMouseUp={() => setIsAnimate(false)}
           >
             <option value="">Filtrar por...</option>
             <option value="PENDENTE">Pendente</option>
             <option value="CONCLUIDO">Aprovado</option>
             <option value="RECUSADO">Recusado</option>
+            <option value="ANALISE">Análise</option>
           </select>
 
           <ChevronDown
             strokeWidth={3}
-            className={`absolute right-2 top-3 size-5 transition ${isAnimate ? 'rotate-180' : ''
-              }`}
+            className={`text-primary-800 absolute right-1.5 top-3.5 size-5 transition-all duration-500 ${
+              isAnimate ? 'rotate-180' : 'rotate-0'
+            }`}
           />
         </div>
       </div>
-
+      
       {/* CARDS */}
       <div className="max-md:min-h-2/5 mt-5 grid w-full grid-cols-3 gap-y-2 max-xl:grid-cols-2 max-md:flex max-md:h-[90%] max-md:flex-col max-md:space-y-4 max-md:overflow-y-auto max-md:pb-32 md:gap-x-3">
         <AnimatePresence>
