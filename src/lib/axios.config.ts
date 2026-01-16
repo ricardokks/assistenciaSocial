f09143cfb16325d882a318491330f816a5cdf5c6
artizin9 <reis08897@gmail.com>
2026-01-16 18:46:42 -0300

    a

--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 })
 
-// https://api.assistenciasocialmassape.com.br
-// http://localhost:4000
+
